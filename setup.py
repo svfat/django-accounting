2250a9524e0642cedad450797b07900fdc4f6b90
Stanislav Fateev <fateevstas@yandex.ru>
2016-08-08 15:21:21 +0300

    Commented Django_Select2_Py3>=4.2.1 from install_requires due to pip-install error

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(name='django-accounting',
           # Used to render the forms
           'django-bootstrap3==4.11.0',
           # Used to improve the forms
-          'Django_Select2_Py3>=4.2.1',
+          #'Django_Select2_Py3>=4.2.1',
           # Used for date/time form fields
           'django-datetime-widget>=0.9,<1.0',
           # Define beautiful tags
